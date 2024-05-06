afcbe8312a5637240d672ca98f2268d282714e0a
oussamasaihi <oussama.saihi@esprit.tn>
2024-05-06 15:44:03 +0100

    migrate

---
+++ AM.Core.Service/IPlaneService.cs
@@ -0,0 +1,17 @@
+﻿using AM.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Core.Service
+{
+    public interface IPlaneService : IService<Plane>
+    {
+        //void Add(Plane p);
+        //void Delete(Plane p);
+
+        //IList<Plane> GetAll();
+    }
+}
---
+++ AM.Core.Service/PlaneService.cs
@@ -0,0 +1,39 @@
+﻿using AM.Core.Domain;
+using AM.Core.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Core.Service
+{
+    public class PlaneService : Service<Plane>, IPlaneService
+    {
+       //IRepository<Plane> planeRepo;
+       // readonly IUnitOfWork unitOfWork; 
+
+        public PlaneService(IUnitOfWork unitOfWork):base(unitOfWork) 
+        {
+            
+            //this.unitOfWork = unitOfWork;
+            //planeRepo = unitOfWork.GetRepository<Plane>();
+        }
+        //public void Add(Plane p)
+        //{
+        //    planeRepo.Add(p);
+        //    unitOfWork.Save();
+        //}
+
+        //public void Delete(Plane p)
+        //{
+        //    planeRepo.Delete(p);
+        //    unitOfWork.Save();
+        //}
+
+        //public IList<Plane> GetAll()
+        //{
+        //    return planeRepo.GetAll();
+        //}
+    }
+}
